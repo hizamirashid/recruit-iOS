263b8aabf7fa055acc3ca342092d409c8d43e446
Hizami Rashid Mac <hizami.rashid.harun@gmail.com>
2022-07-23 23:26:35 +0800

    added function request that use generic parameter to adapt most cases added enum to handle error

---
+++ ASBInterviewExercise/Helpers/Enums.swift
@@ -0,0 +1,13 @@
+//
+//  Enums.swift
+//  ASBInterviewExercise
+//
+//  Created by Mobile Dev on 23/07/2022.
+//
+
+import Foundation
+
+enum CustomError: Error {
+    case invalidUrl
+    case invalidData
+}
--- ASBInterviewExercise/Services/RestClient.swift
+++ ASBInterviewExercise/Services/RestClient.swift
@@ -15,11 +15,41 @@ class RestClient {
         session = URLSession(configuration: .default)
     }
     
-    func apiRequest(_ request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionTask {
+//    func apiRequest(_ request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionTask {
+//
+//        let sessionTask = session.dataTask(with: request, completionHandler: completionHandler)
+//        sessionTask.resume()
+//        return sessionTask
+//    }
+    
+    func request<T: Codable> (url: String, expecting: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
         
-        let sessionTask = session.dataTask(with: request, completionHandler: completionHandler)
-        sessionTask.resume()
-        return sessionTask
+        guard let urlObj = URL(string: url) else { return }
+        var request = URLRequest(url: urlObj)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        let task: URLSessionDataTask = session.dataTask(with: request as URLRequest) { data, response, error in
+            
+            guard let data = data else {
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.failure(CustomError.invalidData))
+                }
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(expecting, from: data)
+                completion(.success(result))
+            }
+            catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
     }
     
     func cancelAllTasks() {
